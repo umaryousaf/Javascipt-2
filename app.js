b8b1248f8561eff44374c3850d9832754fa81ef1
umaryousaf <65030709+umaryousaf@users.noreply.github.com>
2020-06-21 01:16:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -624,13 +624,13 @@
 // stri = prompt("Enter a string")
 
 // function uppercase(str){
-//   var array1 = str.split(' ');
+//   var array1 = str.split(" ");
 //   var newarray1 = [];
     
 //   for(var i = 0; i < array1.length; i++){
 //       newarray1.push(array1[i].charAt(0).toUpperCase()+array1[i].slice(1));
 //   }
-//   return newarray1.join(' ');
+//   return newarray1.join(" ");
 // }
 // console.log(uppercase(stri));
 
